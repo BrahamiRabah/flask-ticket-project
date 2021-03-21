dc760a8ebb1b92072bca27dc1b0a65aac37a4666
brahamirabah <brahamirabah8@gmail.com>
2021-03-21 16:56:53 +0000

    update the html page

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ db_password = os.environ.get('DB_PASSWORD', default='secure_password')
 db_port = os.environ.get('DB_PORT', default='5432')
 db_user = os.environ.get('DB_USERNAME', default='dashboard')
 
-SQLALCHEMY_DATABASE_URI = f"postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
